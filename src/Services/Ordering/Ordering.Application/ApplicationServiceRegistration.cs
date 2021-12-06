78c1e63ddf7646ece54ebe8ed99afd4c40f98e1b
Yaroslav Brichko <yaroslavb@algotec.co.il>
2021-12-06 09:11:07 +0200

    Ordering.API

---
+++ src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
+using System.Reflection;
+
+namespace Ordering.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            // Install-Package AutoMapper.Extensions.Microsoft.DependencyInjection
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            //Install-Package FluentValidation.DependencyInjectionExtensions
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            return services;
+        }
+    }
+}
